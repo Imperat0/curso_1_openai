0729f8078e8bcadb88b30764229c843e9d582129
Imperat0 <imperato06@gmail.com>
2025-08-07 21:49:21 -0300

    laços de repetição para gerar um analisador de sentimentos de demais itens da lista

--- analisador_sentimentos.py
+++ analisador_sentimentos.py
@@ -40,7 +40,7 @@ def analisador_sentimentos(produto):
     """
 
     prompt_usuario = carrega(f"./dados/avaliacoes-{produto}.txt")
-    print(f"Inicou a análise de sentimentos do produto {produto}")
+    print(f"Iniciou a análise de sentimentos do produto {produto}")
 
     lista_mensagens = [
         {
@@ -65,5 +65,6 @@ def analisador_sentimentos(produto):
         print(f"Erro de Autenticação: {e}")
     except openai.APIError as e:
         print(f"Erro de API: {e}")    
-
-analisador_sentimentos("Maquiagem mineral")
+lista_de_produtos = ["Camisetas de algodão orgânico", "Jeans feitos com materiais reciclados", "Maquiagem mineral"]
+for um_produto in lista_de_produtos:
+    analisador_sentimentos(um_produto)
